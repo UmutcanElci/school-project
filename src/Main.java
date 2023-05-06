1119878613ae39c013b01297f1b8501f43318fc2
Umutcan Elçi <umutcanelci@yaani.com>
2023-05-06 17:30:52 +0300

    DBSetup
    DBSetup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Helper.DBServerConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            String query = "SELECT * FROM public.books";
+            Statement st = DBServerConnector.getInstance().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()){
+                System.out.println("Name : " + rs.getString("bookname"));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
