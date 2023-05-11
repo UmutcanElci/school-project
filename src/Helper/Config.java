fec6be439ec60ec563bd3356b817b70dde1f630c
Umutcan Elçi <umutcanelci@yaani.com>
2023-05-11 13:36:22 +0300

    Model and Database Query

--- src/Helper/Config.java
+++ src/Helper/Config.java
@@ -4,4 +4,7 @@ public class Config {
     public static final String PROJECT_NAME = "Library Management System";
     public static final String ADMIN_USERNAME = "admin";
     public static final String ADMIN_PASSWORD = "admin";
+
+    //DB
+    public static final String SQL_GET_ALL = "SELECT * FROM books";
 }
--- src/Model/Manager/BookDao.java
+++ src/Model/Manager/BookDao.java
@@ -1,14 +1,38 @@
 package Model.Manager;
 
+import Helper.Config;
+import Helper.DbConnection;
 import Model.Books;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BookDao {
-    private Books books;
-    public static ArrayList<Books> getAllBooks(){
 
-        return null;
+
+    public static ArrayList<Books> getAllBooks(){
+        ArrayList<Books> arrayList = new ArrayList<>();
+        Books obj;
+        try {
+            Statement st = DbConnection.getInstance().createStatement();
+            ResultSet rs = st.executeQuery(Config.SQL_GET_ALL);
+            while (rs.next()){
+                obj = new Books();
+                obj.setId(rs.getInt("id"));
+                obj.setBookTitle(rs.getString("title"));
+                obj.setBookAuthor(rs.getString("author"));
+                obj.setBookPageCount(rs.getInt("page_count"));
+                obj.setBookVolume(rs.getInt("volume"));
+                arrayList.add(obj);
+            }
+            st.close();
+            rs.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return arrayList;
     }
 
     public static boolean add(Books books){
