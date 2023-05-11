0ebf9aa6bf57c3897e9fb260a1f90c2f74eecd21
Umutcan Elçi <umutcanelci@yaani.com>
2023-05-11 12:02:32 +0300

    Revert "up"
    This reverts commit bd1d1ce45193559be3547e976fc1624ac92e3b86.

--- src/Helper/Config.java
+++ src/Helper/Config.java
@@ -2,6 +2,9 @@ package Helper;
 
 public class Config {
     public static final String PROJECT_NAME = "Library Management System";
+    public static final String DB_URL = "jdbc:postgresql://db-postgresql-fra1-66462-do-user-13616688-0.b.db.ondigitalocean.com:25060/defaultdb";
+    public static final String DB_USERNAME = "doadmin";
+    public static final String DB_PASSWORD = "AVNS_x_Z3T4Upse3HiotDh0O";
     public static final String ADMIN_USERNAME = "admin";
     public static final String ADMIN_PASSWORD = "admin";
 }
