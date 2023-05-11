fe2b51caf9f976ca2a97672df09998e4003bed07
Umutcan Elçi <umutcanelci@yaani.com>
2023-05-11 13:26:31 +0300

    Model and Manager

---
+++ src/Model/Manager/BookDao.java
@@ -0,0 +1,28 @@
+package Model.Manager;
+
+import Model.Books;
+
+import java.util.ArrayList;
+
+public class BookDao {
+    private Books books;
+    public static ArrayList<Books> getAllBooks(){
+
+        return null;
+    }
+
+    public static boolean add(Books books){
+
+        return false;
+    }
+
+    public static boolean update(Books books){
+
+        return false;
+    }
+
+    public static boolean delete(int id){
+
+        return false;
+    }
+}
