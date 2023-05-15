368b39ec89e41df6cb11eaff2a09db7469160d59
uokaradeniz <uokaradeniz@gmail.com>
2023-05-15 16:06:50 +0300

    table update function added

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,9 @@
-import Model.Books;
-import Model.Manager.BookDao;
 import View.TableTest;
-
 import javax.swing.*;
-import java.awt.print.Book;
-import java.sql.*;
 
 public class Main {
-    //jdbc:sqlite:sample.db
     public static void main(String[] args) {
-
-//        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
-//            Statement statement = connection.createStatement();
-//            ResultSet resultSet = statement.executeQuery("SELECT title FROM books");
-//            while (resultSet.next()) {
-//                String title = resultSet.getString("title");
-//                System.out.println(title);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Table Test");
         frame.add(new TableTest().panel1);
         frame.pack();
         frame.setSize(800, 250);
--- src/View/TableTest.java
+++ src/View/TableTest.java
@@ -1,13 +1,62 @@
 package View;
 
+import Helper.Config;
+import Helper.DbConnection;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class TableTest {
-    String header[] = {"ID", "Title", "Author", "Page Count", "Volume"};
     public JPanel panel1;
+    String header[] = {"ID", "Title", "Author", "Page Count", "Volume"};
     private JTable table1;
+    private JPanel addBookPanel;
+    private JTextField titleTextField;
+    private JTextField authorTextField;
+    private JTextField pCountTextField;
+    private JTextField volumeTextField;
+    private JLabel titleLabel;
+    private JLabel authorLabel;
+    private JLabel pCountLabel;
+    private JLabel volumeLabel;
+    private JButton addDataButton;
+    private JTextField idTextField;
+    private JLabel idLabel;
+
+    public TableTest() {
+        addDataButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    PreparedStatement pr = DbConnection.getInstance().prepareStatement(Config.SQL_UPDATE);
+                    pr.setString(1, titleTextField.getText());
+                    pr.setString(2, authorTextField.getText());
+                    pr.setInt(3, Integer.parseInt(pCountTextField.getText()));
+                    pr.setInt(4, Integer.parseInt(volumeTextField.getText()));
+                    pr.setInt(5, Integer.parseInt(idTextField.getText()));
+                    pr.executeUpdate();
+                    DefaultTableModel model = (DefaultTableModel) table1.getModel();
+                    model.setRowCount(0);
+
+                    Statement statement = DbConnection.getInstance().createStatement();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
+                    while (resultSet.next()) {
+                        Object[] row = {resultSet.getString("id"), resultSet.getString("title"),
+                                resultSet.getString("author"), resultSet.getInt("page_count"),
+                                resultSet.getInt("volume")};
+                        model.addRow(row);
+                    }
+
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
