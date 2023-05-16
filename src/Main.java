40beca035454815db2dd4697cb73d7e900fd30a8
uokaradeniz <uokaradeniz@gmail.com>
2023-05-16 22:50:53 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import View.AdminLogin;
+import View.Library;
 import View.TableTest;
+import View.UserLogin;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Table Test");
-        frame.add(new TableTest().panel1);
-        frame.pack();
-        frame.setSize(750, 400);
-        frame.setVisible(true);
+        Library library = new Library();
+//        JFrame frame = new JFrame("Table Test");
+//        frame.add(new TableTest().panel1);
+//        frame.pack();
+//        frame.setSize(750, 400);
+//        frame.setVisible(true);
 
         String query = "SELECT * FROM public.books";
     }
--- src/View/UserLogin.java
+++ src/View/UserLogin.java
@@ -1,12 +1,17 @@
 package View;
 
+import Helper.Config;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class UserLogin extends JFrame {
     private JLabel userLogin;
     private JLabel userName;
     private JTextField userNameTextField1;
-    private JPanel userLoginPanel;
+    public JPanel userLoginPanel;
     private JPasswordField passwordField1;
     private JButton enterButton;
     private JLabel userPassword;
@@ -16,6 +21,19 @@ public class UserLogin extends JFrame {
         setSize(600,400);
         setTitle("Library entrance");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = "test";
+                String password = "test";
+                if(userNameTextField1.getText().equals(username) &&
+                        Arrays.equals(passwordField1.getPassword(), password.toCharArray())) {
+                    JOptionPane.showMessageDialog(null, "Logged In.");
+                } else {
+                    JOptionPane.showMessageDialog(null,"Username or password was incorrect.");
+                }
+            }
+        });
     }
 
 }
